6ae2d808ceb701ddee87be9d1cd3ce2c59a52639
renardbergson <renard.contato@gmail.com>
2025-08-21 23:45:21 -0300

    fix: corrigindo função de formatar datas no formato BR

--- src/app/shared/utils/date.utils.ts
+++ src/app/shared/utils/date.utils.ts
@@ -2,12 +2,14 @@ export function formatDateToIso(stringDate: string): string {
   const [day, month, year] = [
     Number(stringDate.slice(0, 2)),
     Number(stringDate.slice(2, 4)),
-    Number(stringDate.slice(4))
-  ]
-  const date = new Date(year, month-1, day);
+    Number(stringDate.slice(4)),
+  ];
+  const date = new Date(year, month - 1, day);
   return date.toISOString();
 }
 
 export function formatDateToBR(stringDate: string): string {
-  return new Date(stringDate).toLocaleDateString('pt-BR');
+  const dateOnly = stringDate.split('T')[0];
+  const [year, month, day] = dateOnly.split('-');
+  return `${day}/${month}/${year}`;
 }
